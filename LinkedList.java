7a78fafca675c934022ba4f3e7b97df7acba1aa8
VelascoAMath <alfredo0234@gmail.com>
2021-08-03 03:00:01 -0400

    Put the contents of Node.java into LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -257,4 +257,46 @@ public class LinkedList<E> {
 //			System.out.println(a.removeLast());
 //		}
 //	}
+
+
+	/**
+	 * It's a Node for LinkedLists.
+	 * 
+	 * @author CoolerMaster
+	 *
+	 * @param <E>
+	 */
+	class Node<E> {
+		E item;
+		Node<E> next;
+
+		/**
+		 * Makes a node that stores an element.
+		 * 
+		 * @param Eitem
+		 */
+		public Node(E item) {
+			this.item = item;
+			next = null;
+		}
+
+		/**
+		 * Makes a node that connects to another and has an element.
+		 * 
+		 * @param E
+		 *            item
+		 * @param Node<E>
+		 *            nextNode
+		 */
+		public Node(E item, Node<E> nextNode) {
+			this.item = item;
+			next = nextNode;
+		}
+
+		public String toString() {
+			return String.valueOf(item);
+		}
+	}
 }
+
+
