756424c31e4b97679203584caab7722596d21b1b
Alfredo Velasco <alfredo0234@gmail.com>
2023-10-02 23:19:20 -0500

    Added a class that implements IndexCollection using an ArrayList

---
+++ src/velasco/array/IndexList.java
@@ -0,0 +1,35 @@
+package src.velasco.array;
+
+import src.velasco.other.IndexCollection;
+
+import java.util.Iterator;
+
+public class IndexList implements IndexCollection {
+    BArrayList<Integer> indexList;
+
+    IndexList(){
+        indexList = new BArrayList<>();
+    }
+
+    @Override
+    public boolean contains(int i) {
+        return indexList.contains(i);
+    }
+
+    @Override
+    public void add(int i) {
+        if(!contains(i)){
+            indexList.add(i);
+        }
+    }
+
+    @Override
+    public int remove(int i) {
+        return indexList.remove(i);
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return indexList.iterator();
+    }
+}
